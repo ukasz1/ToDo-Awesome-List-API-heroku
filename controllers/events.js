842e9d4ed55d3e1791188cee764543d7a7721261
ukasz1 <lukaszmitkowski53@gmail.com>
2022-05-04 17:03:20 +0200

    data read from db

--- controllers/events.js
+++ controllers/events.js
@@ -1,37 +1,9 @@
-const getAllEvents = async (req, res) => {
+const Event = require('../models/Event')
 
-  res.status(200).json([
-    {
-      id: 1,
-      task: 'Praca',
-      description: 'Raport dla Roberta',
-      day: '31',
-      month: '05',
-      year: '2022',
-      hour: '13',
-      minute: '00'
-    },
-    {
-      id: 2,
-      task: 'Nauka',
-      description: 'Sprawozdanie z fizyki',
-      day: '15',
-      month: '06',
-      year: '2022',
-      hour: '08',
-      minute: '00'
-    },
-    {
-      id: 3,
-      task: 'Nauka',
-      description: 'Korepetycje z chemii',
-      day: '20',
-      month: '07',
-      year: '2022',
-      hour: '11',
-      minute: '00'
-    }
-  ])
+const getAllEvents = async (req, res) => {
+  const events = await Event.find({});
+  console.log(events);
+  res.status(200).json([...events]);
 }
 
 module.exports = {
--- models/Event.js
+++ models/Event.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const EventSchema = new mongoose.Schema({
   task: String,
   description: String,
-  day: Number,
-  month: Number,
-  year: Number,
-  hour: Number,
-  minute: Number,
-  unixTime: Number
+  day: String,
+  month: String,
+  year: String,
+  hour: String,
+  minute: String,
+  unixTime: String
 })
 
 module.exports = mongoose.model('Event', EventSchema)
